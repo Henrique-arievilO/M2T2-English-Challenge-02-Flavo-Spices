dd65a289303ff2e31eb14c11a7015241dfd4d79c
Wanderson Henriqe Oliveira <wandholiveira1@gmail.com>
2022-08-01 21:07:09 -0300

    ✨ feat: new project

---
+++ script.js
@@ -0,0 +1,79 @@
+// const btnIncrement = document.getElementById("increment");
+// const counterResult = document.getElementById("counterResult");
+// const btnDecrement = document.getElementById("decrement")
+// const addCart = document.getElementById("add")
+
+// let counter = 0;
+// let buy = 0;
+
+// btnIncrement.addEventListener('click', function(){
+//     counterResult.innerHTML = ++counter;
+// })
+
+// btnDecrement.addEventListener('click', function(){
+//         if (counterResult.innerHTML > 0){
+//             counterResult.innerHTML = --counter;
+//         } 
+// })
+
+// addCart.addEventListener('click', function(){
+//     btnBag.innerHTML = counterResult.innerHTML;
+// })
+
+
+
+
+const btnIncrement1 = document.getElementById("increment1");
+const counterResult1 = document.getElementById("counterResult1");
+const btnDecrement1 = document.getElementById("decrement1")
+const addCart1 = document.getElementById("add1")
+
+let counter1 = 0;
+
+btnIncrement1.addEventListener('click', function(){
+    counterResult1.innerHTML = ++ counter1;
+})
+
+btnDecrement1.addEventListener('click', function(){
+        if (counterResult1.innerHTML > 0){
+            counterResult1.innerHTML = -- counter1;
+        } 
+})
+
+
+
+const btnIncrement2 = document.getElementById("increment2");
+const counterResult2 = document.getElementById("counterResult2");
+const btnDecrement2 = document.getElementById("decrement2")
+const addCart2 = document.getElementById("add2")
+
+let counter2 = 0;
+
+btnIncrement2.addEventListener('click', function(){
+    counterResult2.innerHTML = ++ counter2;
+})
+
+btnDecrement2.addEventListener('click', function(){
+        if (counterResult2.innerHTML > 0){
+            counterResult2.innerHTML = -- counter2;
+        } 
+})
+
+
+
+const btnIncrement3 = document.getElementById("increment3");
+const counterResult3 = document.getElementById("counterResult3");
+const btnDecrement3 = document.getElementById("decrement3")
+const addCart3 = document.getElementById("add3")
+
+let counter3 = 0;
+
+btnIncrement3.addEventListener('click', function(){
+    counterResult3.innerHTML = ++ counter3;
+})
+
+btnDecrement3.addEventListener('click', function(){
+        if (counterResult3.innerHTML > 0){
+            counterResult3.innerHTML = -- counter3;
+        } 
+})
